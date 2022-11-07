886164ff29ff4d0f8bdc58c2fd587abc124ea20b
Ojo Adebayo <bayoojo1@yahoo.com>
2022-11-07 16:10:58 +0100

    Updated-071122

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -4,13 +4,24 @@
     <div v-if="!tags.length">
         You have no tags. Enter it in the input field below:
     </div>
-    <input type="text" />
+    <div v-for="(tag, index) in tags" :key="tag.id">
+        {{ index + ' : ' + tag }} 
+    </div><br />
+
+    <input 
+    type="text" 
+    :value="newTag" 
+    v-on:keydown.enter="tags.push($event.target.value)"
+    v-on:keydown.tab.prevent="tags.push($event.target.value)"
+    />
+    <!-- <button v-on:click="tags.push(newTag)">OK</button> -->
 </template>
 
 <script>
 export default {
     data: () => ({
         tags: ["vue", "react", "angular"],
+        newTag: "preact"
     }),
 };
 </script>
